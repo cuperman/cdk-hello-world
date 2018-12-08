1aced3cbfe12f0c59900ecf9f375340adbdf0562
Jeff Cooper <jeff@cuperman.net>
2018-12-08 06:38:33 -0800

    Add bucket to the stack

--- lib/hello-world-cdk-stack.ts
+++ lib/hello-world-cdk-stack.ts
@@ -1,9 +1,12 @@
-import cdk = require('@aws-cdk/cdk');
+import cdk = require("@aws-cdk/cdk");
+import s3 = require("@aws-cdk/aws-s3");
 
 export class HelloWorldCdkStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
     super(parent, name, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true
+    });
   }
 }

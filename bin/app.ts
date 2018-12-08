a60e809c964acc1d46c9401131ea618a9631a05a
Jeff Cooper <jeff@cuperman.net>
2018-12-08 09:21:14 -0800

    Add tslint for linting typescript

--- bin/app.ts
+++ bin/app.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-import { App } from "@aws-cdk/cdk";
-import { AppStack } from "../lib/app-stack";
+import { App } from '@aws-cdk/cdk';
+import { AppStack } from '../lib/app-stack';
 
 const app = new App();
-const appName = "HelloWorld";
-const environment = "development";
+const appName = 'HelloWorld';
+const environment = 'development';
 const stackName = `${appName}-${environment}`;
 
 new AppStack(app, stackName);
